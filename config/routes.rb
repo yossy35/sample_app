963de34ca8faab6c72f8563a3028100e8b83c547
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2024-12-18 05:55:17 +0000

    [Finish]アプリ削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   get 'lists/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'list/:id' => 'lists#update', as: 'update_list'
+  resources :lists
 end
